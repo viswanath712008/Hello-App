3d5f76215cecd018cb2684291efa60678a24b4df
viswanath712008 <viswanath622008@gmail.com>
2026-03-28 17:40:08 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,7 @@ package org.example;
 
 
 
-public class Main {
-    public static void main(String[] args) {
-        // Default name
-        String name = "World";
-
-        // Check if a name is provided as a command-line argument
-        if (args.length > 0) {
-            name = args[0]; // Use the provided name
-        }
-
-        System.out.println("Hello, " + name + "!");
+public class Main{
+    public static void main(string[] args) {-
     }
 }
